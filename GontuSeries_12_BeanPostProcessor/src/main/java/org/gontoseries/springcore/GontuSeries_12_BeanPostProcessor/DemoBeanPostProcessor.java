e700e8707f42ce05ae26885a4f5da68bae432112
prasanna4 <prasannawadekar90@gmail.com>
2018-10-29 02:52:06 +0530

    bean post processor

---
+++ GontuSeries_12_BeanPostProcessor/src/main/java/org/gontoseries/springcore/GontuSeries_12_BeanPostProcessor/DemoBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package org.gontoseries.springcore.GontuSeries_12_BeanPostProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DemoBeanPostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization");
+		return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization");
+		return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+	}
+	
+
+}
